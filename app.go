7f8389edb2b085cdbea2089d2ea890355e02e649
Shyamin Ayesh <me@shyamin.com>
2026-05-02 03:42:12 +0530

    Log version on startup

--- app.go
+++ app.go
@@ -29,6 +29,7 @@ func main() {
 	 * Load gateway configuration from file
 	 * Checks CONFIG_PATH env var, defaults to ./config/entriq.yaml
 	 */
+	log.Printf("Entriq gateway version %s", Version)
 	config.AppVersion = Version
 	gatewayConfig, err := config.Load()
 	if err != nil {
